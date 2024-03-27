83a65b1caacfae2fef5e5d28e3ed0266ee2b4ed8
Olga <o.nikolaichenko@ya.ru>
2024-03-27 10:21:43 +0100

    hw3

--- s/3/express/index.js
+++ s/3/express/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("static"));
+const fs = require("fs");
+const path = require("path");
+const pathJSON = path.join(__dirname, "countPage.json");
+
+// app.use(express.static("static"));
 
 // app.get("/", (req, res) => {
 //   res.send(`<h1>Hello</h1>\n<a href="/about">link</a>`);
@@ -10,5 +14,53 @@ app.use(express.static("static"));
 // app.get("/about", (req, res) => {
 //   res.send(`<h1>Hello</h1>\n<a href="/">link</a>`);
 // });
+
+if (!fs.existsSync(pathJSON)) {
+  const countPage = [
+    {
+      page: "/",
+      count: 0,
+    },
+    {
+      page: "/about",
+      count: 0,
+    },
+  ];
+  fs.writeFile(pathJSON, JSON.stringify(countPage, null, 2), (error) => {
+    if (error) return console.log(error);
+  });
+}
+
+app.get("/", (req, res) => {
+  fs.readFile(pathJSON, "utf-8", (error, data) => {
+    if (error) return console.log(error);
+    let dataPage = JSON.parse(data, "utf-8");
+    dataPage[0].count += 1;
+    fs.writeFile(pathJSON, JSON.stringify(dataPage, null, 2), (error) => {
+      if (error) return console.log(error);
+    });
+    res.send(`
+        <h1>Главная страница</h1>
+        <p>Просмотров ${dataPage[0].count}</p>
+        <a href="/about">About</a>
+     `);
+  });
+});
+
+app.get("/about", (req, res) => {
+  fs.readFile(pathJSON, "utf-8", (error, data) => {
+    if (error) return console.log(error);
+    let dataPage = JSON.parse(data, "utf-8");
+    dataPage[1].count += 1;
+    fs.writeFile(pathJSON, JSON.stringify(dataPage, null, 2), (error) => {
+      if (error) return console.log(error);
+    });
+    res.send(`
+        <h1>Страница About</h1>
+        <p>Просмотров ${dataPage[1].count}</p>
+        <a href="/">Главная страница</a>
+     `);
+  });
+});
 
 app.listen(3000);
